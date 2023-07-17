ebd8ec0e475faf31d7b05ac99d50c29d106c631e
angy <angykikonitaphoto@gmail.com>
2023-07-17 07:30:09 -0600

    comenzando

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,17 @@
 // importamos la funcion que vamos a testear
+import { signInWithPopup } from 'firebase/auth';
+import { Login } from '../src/components/Login';
 import { myFunction } from '../src/lib/index';
 
-describe('myFunction', () => {
+describe(Login, () => {
   it('debería ser una función', () => {
-    expect(typeof myFunction).toBe('function');
+    expect(typeof Login).toBe('function');
+  });
+});
+
+
+describe(signInWithPopup, () => {
+  it('debería ser una función', () => {
+    expect(typeof signInWithPopup).toBe('function');
   });
 });
